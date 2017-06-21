02cb8cb686f2eb602d70bd9e0171e90539f44607
Barry <barryboayes17@gmail.com>
2017-06-21 13:00:58 -0700

    first commit

---
+++ app/src/main/java/com/example/newsapp/MainActivity.java
@@ -0,0 +1,83 @@
+package com.example.newsapp;
+
+import android.net.Network;
+import android.os.AsyncTask;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+import android.view.Menu;
+
+import java.io.IOException;
+import java.net.URL;
+
+public class MainActivity extends AppCompatActivity {
+    static final String TAG = "MainActivity";
+    private TextView textView;
+    private ProgressBar progress;
+    //private EditText search;
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main, menu);
+
+        return true;
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        textView = (TextView) findViewById(R.id.news);
+      //  search = (EditText) findViewById(R.id.search);
+        progress = (ProgressBar) findViewById(R.id.progressBar);
+        NetworkTask task = new NetworkTask();
+        task.execute();
+
+
+
+
+
+
+    }
+
+    class NetworkTask extends AsyncTask<URL, Void, String> {
+
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+            progress.setVisibility(View.VISIBLE);
+
+        }
+
+        @Override
+        protected String doInBackground(URL... params) {
+            String result = null;
+            URL url = NetworkUtils.makeURL();
+            Log.d(TAG, "url: " + url.toString());
+            try{
+                result = NetworkUtils.getResponseFromHttpUrl(url);
+            } catch (IOException e){
+                e.printStackTrace();
+            }
+
+            return result;
+        }
+
+        @Override
+        protected void onPostExecute(String s) {
+            super.onPostExecute(s);
+            progress.setVisibility(View.GONE);
+            if(s == null){
+                textView.setText("No News");
+            } else{
+                textView.setText(s);
+            }
+        }
+    }
+}
---
+++ app/src/main/java/com/example/newsapp/NetworkUtils.java
@@ -0,0 +1,58 @@
+package com.example.newsapp;
+
+
+import android.net.Uri;
+import android.util.Log;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.Scanner;
+
+public class NetworkUtils {
+    public static final String TAG = "NetworkUtils";
+    public static final String NEWS_BASE_URL = "https://newsapi.org/v1/articles";
+    public static final String PARAM_SOURCE = "source";
+    public static final String PARAM_SORT_BY = "sortBy";
+    public static final String PARAM_API_KEY = "apiKey";
+
+    public static URL makeURL(){
+
+        Uri uri = Uri.parse(NEWS_BASE_URL).buildUpon()
+                .appendQueryParameter(PARAM_SOURCE, "the-next-web")
+                .appendQueryParameter(PARAM_SORT_BY, "latest")
+                .appendQueryParameter(PARAM_API_KEY, "f9cf08c973bf4c38a5081450258bc8d0").build();
+        URL url = null;
+
+        try{
+            String urlString = uri.toString();
+            Log.d(TAG, "Url: " + urlString);
+            url = new URL(uri.toString());
+        } catch(MalformedURLException e){
+            e.printStackTrace();
+        }
+
+        return url;
+    }
+
+    public static String getResponseFromHttpUrl(URL url) throws IOException{
+        HttpURLConnection urlConnection = (HttpURLConnection) url.openConnection();
+        try{
+            InputStream in = urlConnection.getInputStream();
+
+            Scanner scanner = new Scanner(in);
+            scanner.useDelimiter("\\A");
+
+            boolean hasInput = scanner.hasNext();
+            if(hasInput){
+                return scanner.next();
+            } else {
+                return null;
+            }
+        } finally {
+            urlConnection.disconnect();
+        }
+    }
+}
